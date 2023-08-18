a8d8d3efbfc5b004f0535b4dbed7a17b2d25cec4
arsalmkhawaja <arsalkh62@gmail.com>
2023-08-18 15:18:09 +0500

    ...

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import image1 from "../assets/StampsCarouselImages/2022-08-15-Stamp-Diamond-Jubilee-Of-Pakistan-1947-2022-Commemorative-Postage-Stamp-August-14-2022.jpg"
+import { Text } from '@chakra-ui/react';
 const AboutUs = () => {
   return (
     <div className="about-us-container">
       <div className="background-image" >
-        <img src={image1} alt="About us" className="object-cover w-full opacity-50 h-full" />
+        <img src={image1} alt="About us" className="object-cover w-[100%] opacity-80 h-[50vh]" />
+        <Text
+          fontSize={"5xl"}
+          fontWeight="bold"
+          color={"white"}
+          bgColor={"#ed1b24"}
+          p={"5"}
+          borderRadius={"none"}
+          transform="skewX(-10deg)"
+        >
+          About Us
+        </Text>
       </div>
       <div className="history-section">
         {/* History section content */}
