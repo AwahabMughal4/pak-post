16ccad4a8508de7eb2041bebecbe2bbbd004893d
Eisha-Farooq <eishafarooq5@gmail.com>
2023-09-07 09:15:23 -0700

    .

--- src/DataObjects/NewsTenders.jsx
+++ src/DataObjects/NewsTenders.jsx
@@ -1,3 +1,69 @@
+import img1 from "../assets/NewsTender/Tenders/2023-06-19-Tender-Notice-For-Transportation-conveyance-of-mail-State-Mail-sme-Under-The-PMG-NP-RWP.jpg";
+import img2 from "../assets/NewsTender/Tenders/2023-06-22-Tender-Notice-For-Supply-of-Stationery-Itemsl-Under-The-PMG-NS-Circle-Hyd.jpg";
+import img3 from "../assets/NewsTender/Tenders/2023-06-26-Tender-Notice-For-Supply-of-Stationery-Other-Itemsl-Under-The-PMG-FC-GB-IBD.jpg";
+import img4 from "../assets/NewsTender/Tenders/2023-07-10-Tender-Notice-for-Transportation-of-Annual-maintenance-2023-24-Under-The-dgppo-ibd-eng.jpg";
+import img5 from "../assets/NewsTender/Tenders/2023-07-11-Tender-Notice-for-Repair-Maintenance-of-Cooling-Tower-Under-The-dgppo-ibd.jpg";
+import img6 from "../assets/NewsTender/Tenders/2023-07-14-Tender-Notice-For-Purchase-of-Stationery-Under-The-PMG-Southern-PJ-MLT.jpeg";
+import img7 from "../assets/NewsTender/Tenders/2023-07-17-Pre-Qualification-Notice-For-Civil-Work-Under-The-PMG-Southern-PJ-MLT.jpeg";
+import img8 from "../assets/NewsTender/Tenders/2023-07-17-Tender-Notice-for-Supply-of-Stationery-Items-Under-The-PMG-LHR.jpg";
+import pdf1 from "../assets/NewsTender/Tenders/2023-07-17-Tender-Notice-For-Supply-of-Stationery-Under-The-PMG-Khi.pdf";
+import img9 from "../assets/NewsTender/Tenders/2023-07-17-Tender-Notice-Regarding-Security-Guards-Services-Under-The-PMG-Khi-eng.jpg"
+import pdf2 from "../assets/NewsTender/Tenders/2023-07-18-Tender-Notice-for-Pre-Qualification-of-Vehicles-Workshops-Under-The-dgppo-ibd.pdf";
+import img10 from "../assets/NewsTender/Tenders/2023-07-20-Pre-Qualification-Notice-for-Construction-Repair-Under-The-PMG-KPK-eng.jpeg"
+import img11 from "../assets/NewsTender/Tenders/2023-07-20-Tender-Notice-for-Mail-Transmission-of-Six-Mail-Lines-Under-The-PMG-KPK.jpg"
+import img12 from "../assets//NewsTender/Tenders/2023-07-24-Pre-Qualification-od-Civil-Work-Construction-2023-24-Under-The-PMG-Khi.jpg"
+import img13 from "../assets/NewsTender/Tenders/2023-07-24-Tender-Notice-for-Purchase-of-Stationary-Items-Under-The-PMG-KPK.jpg"
+import img14 from "../assets/NewsTender/Tenders/2023-07-24-Tender-Notice-of-13-SMES-For-Transmission-of-Mail-Under-The-PMG-LHR.jpg"
+import img15 from "../assets/NewsTender/Tenders/2023-07-26-Tender-Notice-For-Purchase-of-Stationery-Items-Under-The-PMG-NP-RWP.jpg"
+import img16 from "../assets/NewsTender/Tenders/2023-07-31-Pre-Qualification- Notice-for-Repair-and-Maintenance-Under-The-PMG-NS-Circle-Hyd.jpg"
+import pdf3 from "../assets/NewsTender/Tenders/2023-07-31-Tender-Notice-For-Security-Service-Under-The-PMG-NS-Circle-Hyd.pdf"
+import img17 from "../assets/NewsTender/Tenders/2023-08-02-Pre-Qualification-Notice-for-Construction-Repair-Under-The-PMG-RWP-eng.jpg"
+import img18 from "../assets/NewsTender/Tenders/2023-07-20-Tender-Notice-for-Mail-Transmission-of-Six-Mail-Lines-Under-The-PMG-KPK.jpg"
+import pdf4 from "../assets/NewsTender/Tenders/2023-08-18-Pre-Qualification-Notice-for-Construction-Repair-Under-The-PMG-IBD.pdf"
+import img19 from "../assets/NewsTender/Tenders/2023-08-30-Tender-Notice-for-Purchase-of-Stationery-Items-Under-The-dgppo-ibd.jpg"
+import img20 from "../assets/NewsTender/Tenders/2023-09-04-Tender-Notice-for-Purchase-of-Other-Misc-Items-Under-The-dgppo-ibd.jpg"
+
+// NEWS
+import image1 from "../assets/NewsTender/News/2020-01-06-49th-International-Letter-Writing-Competition-for-Young-People-2020.jpg"
+import PDF1 from "../assets/NewsTender/News/2020-02-07-Announcement-of-ISDB-Scholarship-Programmes-for-the-Academic-Year-2020-21.pdf"
+import image2 from "../assets/NewsTender/News/2020-06-23-Printing-And-Supply-Of-500000-Security-Featured-Withdrawal-Slips-For-Savings-Bank-Ordinary-And-Special-Account-Under-The-Directorate-General-Pakistan-Post-Office-.jpg"
+import image3 from "../assets/NewsTender/News/2020-07-08-Restricting-Participation-of-Institutional-Investors-in-National-Savings-Schemes.jpg"
+import PDF2 from "../assets/NewsTender/News/2020-11-20-Posting-Uploading-Contract-Award-of-Value-Above-Rupees-Fifty-Million-50-Million-On-PPRA-Website.pdf"
+import image4 from "../assets/NewsTender/News/2021-01-01-Delays-in-the-Delivery-of-P-O-Mail-Abroad-Due-to-COVID-19.jpg"
+import PDF3 from "../assets/NewsTender/News/2021-01-27-Announcement-of-ISDB-Scholarship-Programmes-for-the-Academic-Year-2021-22.pdf"
+import image5 from "../assets/NewsTender/News/2021-06-07-ums-umo-ums-cod-service-started-at-jandanwala-post-office-order-by-dg-islamabad.jpg"
+import image6 from "../assets/NewsTender/News/2021-06-18-ums-umo-ums-cod-service-started-at-khanewal-post-office-order-by-dg-islamabad.jpg"
+import PDF4 from "../assets/NewsTender/News/2021-06-23-Amazon-Fulfilment-and-Facilitation-Centres-For-All-GPOS.pdf"
+import PDF5 from "../assets/NewsTender/News/2021-07-29-Tender-Notice-Regarding-Baseline-Contract-EMTTS-IPS-MDUS-CMS-Under-The-DGPPO-IBD.pdf"
+import image7 from "../assets/NewsTender/News/2021-08-07-ums-umo-ums-cod-service-started-at-khairpur-post-office-order-by-dg-islamabad.jpg"
+import image8 from "../assets/NewsTender/News/2021-09-22-Annual-Procurement-Plan-of-Pakistan-Post-For-The-Financial-Year-2021-22.jpg"
+import image9 from "../assets/NewsTender/News/2021-08-23-ums-umo-ums-cod-service-started-at-wan-bhachran-post-office-order-by-dg-islamabad.jpg"
+import image10 from "../assets/NewsTender/News/2021-08-25-Provision-of-Insurance-on-Express-Post-Articles-UMS-and-UMS-COD-Order-By-DG-Islamabad (1).jpg"
+import image11 from "../assets/NewsTender/News/2021-08-25-Provision-of-Insurance-on-Express-Post-Articles-UMS-and-UMS-COD-Order-By-DG-Islamabad.jpg"
+import PDF6 from "../assets/NewsTender/News/2021-09-06-Contract-Award-Proforma-I-and-II-Under-The-DGPPO-IBD.pdf"
+import image12 from "../assets/NewsTender/News/2021-09-16-ums-umo-ums-cod-service-started-at-Industrial-Estate-Multan-post-office-order-by-dg-islamabad.jpg"
+import image13 from "../assets/NewsTender/News/2021-09-22-Annual-Procurement-Plan-of-Pakistan-Post-For-The-Financial-Year-2021-22.jpg"
+import image14 from "../assets/NewsTender/News/2021-10-05-afz-ums-umo-ums-cod-service-started-at-Qutab-Pur-Lodhran-post-office-order-by-dg-islamabad (1).jpg"
+import image15 from "../assets/NewsTender/News/2021-10-05-afz-ums-umo-ums-cod-service-started-at-Qutab-Pur-Lodhran-post-office-order-by-dg-islamabad.jpg"
+import PDF7 from "../assets/NewsTender/News/2021-11-18-Annual-Procurement-Plan-of-Pakistan-Post-For-The-Financial-Year-2021-22.pdf"
+import PDF8 from "../assets/NewsTender/News/2021-11-19-List-of-Prohibited-Items-According-To-Pakistan-Post-Guide.pdf"
+import image16 from "../assets/NewsTender/News/2022-01-12-EP-Services-ums-umo-ums-cod-service-started-at-Mankera-Bhakkar-post-office-order-by-dir-EPS-Ibd.jpg"
+import PDF9 from "../assets/NewsTender/News/2022-02-19-Gazette-Of-Pakistan-S-R-O-S-under-the-DGPPO-IBD.pdf"
+import image17 from "../assets/NewsTender/News/2022-03-09-51st-International-Letter-Writing-Competition-for-Young-People-2022.jpg"
+import PDF10 from "../assets/NewsTender/News/2022-04-21-Gazette-Of-Pakistan-S-R-O-S-under-the-DGPPO-IBD.pdf"
+import PDF11 from "../assets/NewsTender/News/2022-05-14-ums-umo-ums-cod-service-started-at-Muzaffarabad-order-by-dir-ips-islamabad.pdf"
+import PDF12 from "../assets/NewsTender/News/2022-05-21-Tender-Notice-For-EMS-PLUS-Service-Under-The-DGPPO-IBD.pdf"
+import image18 from "../assets/NewsTender/News/2022-06-02-Announcement-of-Evaluation-Report-PPRA-Rules-35-Under-The-DG-PPO-Islamabad.jpg"
+import PDF13 from "../assets/NewsTender/News/2022-08-30-ums-umo-ums-cod-service-started-at-Muzaffarabad-order-by-dir-ips-islamabad.pdf"
+import PDF14 from "../assets/NewsTender/News/2022-11-04-Procurement-Plan-For-Year-2022-2023-DGPPO-IBD.pdf"
+import PDF15 from "../assets/NewsTender/News/2023-01-11-Validated-EMS-Export-Standards-For-Pakistan.pdf"
+import PDF16 from "../assets/NewsTender/News/2023-02-03-Public-Notice-For-Recruitment-of-ASPOS-MST-Examination-Schedule.pdf"
+import PDF17 from "../assets/NewsTender/News/2023-06-08-ums-umo-ems-service-started-at-Dugal-Sialkot-PO-order-by-dg-islamabad.pdf"
+import image19 from "../assets/NewsTender/News/2023-08-08-DG-PPODG-E-Kachehr.jpeg"
+import image20 from "../assets/NewsTender/News/Capture3.PNG"
+
+
+
 export const TendersData = [
   {
     id: 1,
@@ -493,7 +559,7 @@ export const TendersData = [
     url5: "",
     title6: "",
     url6: "",
-  },
+  }
 ];
 
 export const NewsData = [
