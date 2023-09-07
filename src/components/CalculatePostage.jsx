54c06b9ac5a49bce9932f5fd24dcca789c0cc51d
Rohail-Suii <125988921+Rohail-Suii@users.noreply.github.com>
2023-09-07 21:55:01 +0500

    calculator working

--- src/components/CalculatePostage.jsx
+++ src/components/CalculatePostage.jsx
@@ -1,144 +1,406 @@
 import React, { useState } from "react";
+import { Button } from '@chakra-ui/react'
+const CalculatePostage = () => {
+  const [formData, setFormData] = useState({
+    value: "0",
+    type: "0",
+    ParcelType: "0",
+  });
 
-const DropdownBox = () => {
-  const [selectedOption1, setSelectedOption1] = useState("");
-  const [selectedOption2, setSelectedOption2] = useState("");
-  const [selectedOption3, setSelectedOption3] = useState("");
-  const [selectedOption4, setSelectedOption4] = useState("");
-  const [textFieldValue, setTextFieldValue] = useState("");
-  const [iseps, setIseps] = useState(true);
-  const [isefs, setefs] = useState(false);
+  const [articleWeight, setArticleWeight] = useState("0"); // Changed initial state to a string
+  const [clientType, setClientType] = useState("0");
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [result, setResult] = useState(0);
+  const [isUms, setUms] = useState(true);
+  const [isCod, setCod] = useState(false);
+  const [isFmo, setFmo] = useState(false);
+  const [isUmo, setUmo] = useState(false);
+  const [isclient0, setclient0] = useState(true);
+  const [isclient1, setclient1] = useState(false);
+  const [isclient2, setclient2] = useState(false);
+  const [isclient3, setclient3] = useState(false);
+  const [isclient4, setclient4] = useState(false);
+  const [isclient5, setclient5] = useState(false);
+  const [isclient6, setclient6] = useState(false);
+  const [isclient7, setclient7] = useState(false);
 
-  const handleEPS = () => {
-    setIseps(true);
-    setefs(false);
-  };
-  const handleEFS = () => {
-    setIseps(false);
-    setefs(true);
-  };
-  const handleOption1Change = (event) => {
-    setSelectedOption1(event.target.value);
-  };
-
-  const handleOption2Change = (event) => {
-    setSelectedOption2(event.target.value);
-  };
-
-  const handleOption3Change = (event) => {
-    setSelectedOption3(event.target.value);
-  };
-
-  const handleOption4Change = (event) => {
-    setSelectedOption4(event.target.value);
-  };
-
-  const handleTextFieldChange = (event) => {
-    setTextFieldValue(event.target.value);
+  const handleArticle = (e) => {
+    const { name, value } = e.target;
+    if (name === "value") {
+      setArticleWeight(value);
+    }
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = () => {
-    // Handle form submission here
-  };
-  const [selectedValue, setSelectedValue] = useState("");
+  const handleClient = (e) => {
+    const { value } = e.target;
+    setClientType(value);
+    switch (value) {
+      case "0":
+        setclient0(true);
+        setclient1(false);
+        setclient2(false);
+        setclient3(false);
+        setclient4(false);
+        setclient5(false);
+        setclient6(false);
+        setclient7(false);
+        break;
+      case "1":
+        setclient0(false);
+        setclient1(true);
+        setclient2(false);
+        setclient3(false);
+        setclient4(false);
+        setclient5(false);
+        setclient6(false);
+        setclient7(false);
+        break;
+      case "2":
+        setclient0(false);
+        setclient1(false);
+        setclient2(true);
+        setclient3(false);
+        setclient4(false);
+        setclient5(false);
+        setclient6(false);
+        setclient7(false);
+        break;
+      case "3":
+        setclient0(false);
+        setclient1(false);
+        setclient2(false);
+        setclient3(true);
+        setclient4(false);
+        setclient5(false);
+        setclient6(false);
+        setclient7(false);
+        break;
+      case "4":
+        setclient0(false);
+        setclient1(false);
+        setclient2(false);
+        setclient3(false);
+        setclient4(true);
+        setclient5(false);
+        setclient6(false);
+        setclient7(false);
+        break;
+      case "6":
+        setclient0(false);
+        setclient1(false);
+        setclient2(false);
+        setclient3(false);
+        setclient4(false);
+        setclient5(false);
+        setclient6(true);
+        setclient7(false);
+        break;
+      case "7":
+        setclient0(false);
+        setclient1(false);
+        setclient2(false);
+        setclient3(false);
+        setclient4(false);
+        setclient5(false);
+        setclient6(false);
+        setclient7(true);
+        break;
 
-  const handleSelectChange = (event) => {
-    const value = event.target.value;
-    setSelectedValue(value);
-
-    if (value === "EPS") {
-      handleEPS();
-    } else if (value === "EFS") {
-      handleEFS();
+      // Add cases for other values as needed
+      default:
+        setclient0(true);
+        setclient1(false);
+        setclient2(false);
+        setclient3(false);
+        setclient4(false);
+        setclient5(false);
+        setclient6(false);
+        setclient7(false);
+        break;
     }
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmit(true);
+
+    let postageResult = 0;
+
+    if (isclient0) {
+      // Check if isUms is true for client type 0
+      if (articleWeight >= 0 && articleWeight <= 250) {
+        postageResult = 55;
+      } else if (articleWeight > 250 && articleWeight <= 500) {
+        postageResult = 87;
+      } else if (articleWeight > 500) {
+        const additionalWeight = articleWeight - 500;
+        const additionalCharge = Math.ceil(additionalWeight / 500) * 18;
+        postageResult = 122 + additionalCharge;
+      }
+    } else if (isclient1) {
+      // Check if isCod is true for client type 1
+      if (articleWeight >= 0 && articleWeight <= 250) {
+        postageResult = 87;
+      } else if (articleWeight > 250 && articleWeight <= 500) {
+        postageResult = 122;
+      } else if (articleWeight > 500) {
+        const additionalWeight = articleWeight - 500;
+        const additionalCharge = Math.ceil(additionalWeight / 500) * 35;
+        postageResult = 122 + additionalCharge;
+      }
+    } else if (isclient2) {
+      // Check if isUms is true for client type 2
+      if (articleWeight >= 0 && articleWeight <= 250) {
+        postageResult = 55;
+      } else if (articleWeight > 250 && articleWeight <= 500) {
+        postageResult = 87;
+      } else if (articleWeight > 500) {
+        const additionalWeight = articleWeight - 500;
+        const additionalCharge = Math.ceil(additionalWeight / 500) * 18;
+        postageResult = 122 + additionalCharge;
+      }
+    } else if (isclient3) {
+      // Check if isCod is true for client type 3
+      if (articleWeight >= 0 && articleWeight <= 250) {
+        postageResult = 87;
+      } else if (articleWeight > 250 && articleWeight <= 500) {
+        postageResult = 122;
+      } else if (articleWeight > 500) {
+        const additionalWeight = articleWeight - 500;
+        const additionalCharge = Math.ceil(additionalWeight / 500) * 35;
+        postageResult = 122 + additionalCharge;
+      }
+    } else if (isclient4) {
+      // Check if isFmo is true for client type 4
+      if (articleWeight >= 0 && articleWeight <= 25000) {
+        postageResult = 150;
+      } else if (articleWeight > 25000 && articleWeight <= 50000) {
+        postageResult = 300;
+      } else if (articleWeight > 50000) {
+        alert("Value exceeds 50000 limit");
+      }
+    } else if (isclient6) {
+      // Check if isUmo is true for client type 6
+      if (articleWeight > 0 && articleWeight <= 10000) {
+        postageResult = 175;
+      } else if (articleWeight > 10000 && articleWeight <= 20000) {
+        postageResult = 200;
+      } else if (articleWeight > 20000 && articleWeight <= 50000) {
+        postageResult = 225;
+      } else if (articleWeight > 50000 && articleWeight <= 100000) {
+        postageResult = 250;
+      }
+    } else if (isclient7) {
+      // Check if isUmo is true for client type 7
+      if (articleWeight > 0 && articleWeight <= 10000) {
+        postageResult = 135;
+      } else if (articleWeight > 10000 && articleWeight <= 20000) {
+        postageResult = 160;
+      } else if (articleWeight > 20000 && articleWeight <= 50000) {
+        postageResult = 185;
+      } else if (articleWeight > 50000 && articleWeight <= 100000) {
+        postageResult = 210;
+      }
+    }
+
+    setResult(postageResult);
+
+    // You can now access the articleWeight and clientType for calculations
+    console.log("Article Weight:", articleWeight);
+    console.log("Client Type:", clientType);
+    console.log("Postage Result:", postageResult);
+
+    // Handle form submission here, e.g., send data to the server
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    setIsSubmit(false);
+    setArticleWeight("0"); // Reset the article weight when the form is reset
+    setResult(0); // Reset the result
+  };
+
   return (
-    <div className="p-4 border rounded-lg shadow-md">
-      <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Select a service
-        </label>
-        <select
-          className="w-full p-2 border rounded-md"
-          value={selectedValue}
-          onChange={handleSelectChange}
+    <div>
+      <div className="button flex flex-row space-x-5 justify-center items-center p-5">
+        <Button colorScheme=' red' size='xl'
+          onClick={() => {
+            setUms(true);
+            setCod(false);
+            setFmo(false);
+            setUmo(false);
+            setclient0(true);
+            setclient1(false);
+            setclient2(false);
+            setclient3(false);
+            setclient4(false);
+            setclient5(false);
+            setclient6(false);
+            setclient7(false);
+          }}
         >
-          <option value="EPS">Express Post Services</option>
-          <option value="EFS">Express Financial Services</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Select the type of service
-        </label>
-        {iseps && (
-          <select
-            className="w-full p-2 border rounded-md"
-            value={selectedOption1}
-            onChange={handleOption2Change}
-          >
-            <option value="UMS">Urgent Mail Service (UMS)</option>
-            <option value="EMS">Express Mail Service(EMS)</option>
-            <option value="COD">Cash on Delivery (COD)</option>
-          </select>
-        )}
-        {isefs && (
-          <select
-            className="w-full p-2 border rounded-md"
-            value={selectedOption1}
-            onChange={handleOption2Change}
-          >
-            <option value="FMO">Fax Money Order (FMO)</option>
-            <option value="UMO">Urgent Money Order (UMO)</option>
-          </select>
-        )}
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Select Type of Client
-        </label>
-        <select
-          className="w-full p-2 border rounded-md"
-          value={selectedValue}
-          onChange={handleSelectChange}
+          UMS
+        </Button>
+        <Button
+          onClick={() => {
+            setUms(false);
+            setCod(true);
+            setFmo(false);
+            setUmo(false);
+            setclient0(false);
+            setclient1(false);
+            setclient2(true);
+            setclient3(false);
+            setclient4(false);
+            setclient5(false);
+            setclient6(false);
+            setclient7(false);
+          }}
         >
-          <option value="RC">Regular Client</option>
-          <option value="LRC">Local Regular Client</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Select Country to Ship to (only for EMS)
-        </label>
-        <select
-          className="w-full p-2 border rounded-md"
-          value={selectedValue}
-          onChange={handleSelectChange}
+          COD
+        </Button>
+        <Button
+          onClick={() => {
+            setUms(false);
+            setCod(false);
+            setFmo(true);
+            setUmo(false);
+            setclient0(false);
+            setclient1(false);
+            setclient2(false);
+            setclient3(false);
+            setclient4(true);
+            setclient5(false);
+            setclient6(false);
+            setclient7(false);
+          }}
         >
-          <option value="RC">Country</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Enter Weight in grams.
-        </label>
-        <input
-          type="text"
-          className="w-full p-2 border rounded-md"
-          value={textFieldValue}
-          onChange={handleTextFieldChange}
-        />
-      </div>
-      <div className="flex justify-center">
-        <button
-          className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
-          onClick={handleSubmit}
+          FMO
+        </Button>
+        <Button
+          onClick={() => {
+            setUms(false);
+            setCod(false);
+            setFmo(false);
+            setUmo(true);
+            setclient0(false);
+            setclient1(false);
+            setclient2(false);
+            setclient3(false);
+            setclient4(false);
+            setclient5(false);
+            setclient6(true);
+            setclient7(false);
+          }}
         >
-          Submit
-        </button>
+          UMO
+        </Button>
       </div>
+      <form
+        name="FrontPage_Form1"
+        action=""
+        onSubmit={handleSubmit}
+        className="max-w-md  m-auto bg-slate-50 hover:bg-slate-100 shadow-lg border border-slate-300 rounded-t-md"
+      >
+        <div className="flex flex-row items-center justify-center py-2 space-x-6 shadow-lg bg-[color:var(--primary-color)]  rounded-t-md">
+
+
+        </div>
+        <div className="flex justify-center shadow-xs">
+          <div className="px-2 py-2 mt-2 text-lg font-semibold shadow-sm">Calculate EMS Postage</div>
+          {/* <div className="flex ">
+            <a href="../calculatepostage.jsx" className="font-sm " >Back</a>
+          </div> */}
+        </div>
+        <div>
+          <table className="m-auto mt-2">
+            <tbody className="p-2 ">
+              <tr >
+                <td colSpan="2" className="font-semibold Head text-md">
+                  Calculate Postage:
+                </td>
+              </tr>
+              <tr >
+                <td className="font-base">
+                  Article weight
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="value"
+                    value={formData.value}
+                    onChange={handleArticle}
+                    size="20"
+                    maxLength="5"
+                    className="w-full mt-4 border rounded border-slate-300"
+                  />
+                  <span className="text-sm font-semibold text-blue-600 font-base">
+                    (grams)
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-1 font-normal"> Client Type
+                </td>
+                <td>
+                  <select
+                    name="type"
+                    size="1"
+                    onChange={handleClient}
+                    className="w-full mt-3 border border-gray-300 rounded"
+                  >
+                    {isUms && (
+                      <>
+                        <option value="0"> Local Regular Client</option>
+                        <option value="1" >Regular Client</option>
+                      </>
+                    )}
+                    {isCod && (
+                      <>
+                        <option value="2" >Local Regular Client</option>
+                        <option value="3" >Regular Client</option>
+                      </>
+                    )}
+                    {isFmo && (
+                      <>
+                        <option value="4" >FMO Tariff</option>
+                      </>
+                    )}
+                    {isUmo && (
+                      <>
+                        <option value="6" >Regular Client (City)</option>
+                        <option value="7" >Regular Client (Local)</option>
+                      </>
+                    )}
+                  </select>
+                </td>
+              </tr>
+              <tr >
+                <td colSpan="2" align="right">
+                  <input
+                    type="submit"
+                    name="Submit2"
+                    value="Submit"
+                    className="px-3 py-1 mt-3 mr-2 border rounded shadow-lg hover:bg-slate-200 border-slate-300"
+                  />
+                  <input
+                    type="reset"
+                    name="Reset"
+                    onClick={handleReset}
+                    value="Reset"
+                    className="px-3 py-1 mt-2 mb-3 mr-2 border rounded shadow-lg hover:bg-slate-200 border-slate-300"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </form>
+
+      {isSubmit && <div>{result}</div>}
     </div>
   );
 };
 
-export default DropdownBox;
+export default CalculatePostage;
